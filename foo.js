4a93f2a4637243722852c7a1ad36109e27f64346
GradedJestRisk <graded.jest.risk@gmail.com>
2020-09-17 09:12:04 +0200

    Add incorrect indent code sample from eslint doc

---
+++ foo.js
@@ -0,0 +1,25 @@
+// https://eslint.org/docs/rules/indent#rule-details
+// This rule enforces a consistent indentation style. The default style is 4 spaces.
+
+// Below a code sample from section "Examples of incorrect code for this rule with the default options"
+
+// Code sample has 2 spaces indent, while default is 4 spaces
+// https://github.com/eslint/eslint/blob/master/lib/rules/indent.js#L633
+
+// Lint this code should throw error then, but does not
+// npx eslint foo.js
+
+// Explicitly specifying the rule works well
+// npx eslint --rule 'indent: ["error", 4]' ./foo.js does throw error
+// $ 11:1 error  Expected indentation of 4 spaces but found 2  indent
+
+let  a, b, c, e, f ;
+if (a) {
+  // eslint-disable-next-line no-unused-vars
+  b=c;
+  // eslint-disable-next-line no-inner-declarations,no-unused-vars
+  function foo(d) {
+    // eslint-disable-next-line no-unused-vars
+    e=f;
+  }
+}
