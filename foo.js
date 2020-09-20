cad4848874e79b31722b7ef81a95c06a5a5e36e4
GradedJestRisk <graded.jest.risk@gmail.com>
2020-09-20 12:47:12 +0200

    Add eslint-recommended source code

--- foo.js
+++ foo.js
@@ -1,10 +1,15 @@
-// https://eslint.org/docs/rules/indent#rule-details
-// This rule enforces a consistent indentation style. The default style is 4 spaces.
+// indent-rule default
+// - documentation https://eslint.org/docs/rules/indent#rule-details
+//   "This rule enforces a consistent indentation style. The default style is 4 spaces."
+// - code  https://github.com/eslint/eslint/blob/master/lib/rules/indent.js#L633
+//         let indentType = "space";
+//         let indentSize = 4;
+
+// eslint-recommended configuration does not mention indent-rule
+// -code https://github.com/eslint/eslint/blob/master/conf/eslint-recommended.js
 
 // Below a code sample from section "Examples of incorrect code for this rule with the default options"
-
 // Code sample has 2 spaces indent, while default is 4 spaces
-// https://github.com/eslint/eslint/blob/master/lib/rules/indent.js#L633
 
 // Lint this code should throw error then, but does not
 // npx eslint foo.js
